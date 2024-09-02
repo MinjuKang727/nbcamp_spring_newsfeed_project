282d8502152d37adb7d4d02a0658e0fce5bf2ee1
areum0116 <areum0116@naver.com>
2024-09-02 17:02:29 +0900

    init

---
+++ newsfeed_project/src/test/java/org/sparta/newsfeed_project/NewsfeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.newsfeed_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
