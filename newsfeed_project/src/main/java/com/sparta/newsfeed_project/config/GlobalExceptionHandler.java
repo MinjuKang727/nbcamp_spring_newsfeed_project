2a6866ce94760fdd94c8989461e99dd20dac7ead
areum0116 <areum0116@naver.com>
2024-09-04 12:12:21 +0900

    [fix] GlobalExceptionHandler

--- newsfeed_project/src/main/java/com/sparta/newsfeed_project/config/GlobalExceptionHandler.java
+++ newsfeed_project/src/main/java/com/sparta/newsfeed_project/config/GlobalExceptionHandler.java
@@ -7,13 +7,5 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
-    public ApiResponse<?> getIllegalArgumentException(IllegalArgumentException e) {
-        return ApiResponse.error(e.getMessage(), HttpStatus.BAD_REQUEST.value());
-    }
 
-    @ExceptionHandler
-    public ApiResponse<?> getEtcErrorResponse(Exception e) {
-        return ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
-    }
 }
